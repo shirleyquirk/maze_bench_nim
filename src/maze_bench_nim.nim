1d76700c0bf6aaf8dffdd9b8e411c6d49fd84efd
Benjamin Shirley-Quirk <benjamin.shirley-quirk@geoslam.com>
2021-12-15 21:44:12 +0000

    b

--- src/maze_bench_nim.nim
+++ src/maze_bench_nim.nim
@@ -9,42 +9,37 @@ import
   mazesolver
 
 proc main() =
-
   let image = readImage("maze1000.png")
   echo fmt"Loaded image: {image.width} x {image.height}"
 
   echo "Transforming image into maze..."
-  var now = getMonoTime()
-
-  var maze = initSeq2D(image.width, image.height)
+  var 
+    now = getMonoTime()
+    maze = initSeq2D(image.width, image.height)
   
   for i, rgba in image.data:
     maze[i] = (rgba.r + rgba.g + rgba.b) > 0
  
   echo fmt"Transformed image into maze in {getMonoTime() - now}"
 
-  const 
-    start = (0, 1)
-    finish = (2000, 1999)
-
   func toIdx(p:(int,int)):Point = (p[0] + p[1] * image.width,p[0],p[1])
   let
-    st_idx = start.toIdx
-    fn_idx = finish.toIdx
+    start = (0, 1).toIdx
+    finish = (2000, 1999).toIdx
 
-  echo fmt"Solving maze from ({start[0]}, {start[1]}) to ({finish[0]}, {finish[1]}) ..."
+  echo fmt"Solving maze from ({start.x}, {start.y}) to ({finish.x}, {finish.y}) ..."
   
   # Test "cold"
   now = getMonoTime()
-  var success = maze.isReachable(st_idx, fn_idx)
+  var success = maze.isReachable(start, finish)
   let coldTime = getMonoTime() - now
   # Warm up
   for _ in 0..1000:
-    discard maze.isReachable(st_idx, fn_idx)
+    discard maze.isReachable(start, finish)
 
   # Test "warm"
   now = getMonoTime()
-  success = maze.isReachable(st_idx, fn_idx)
+  success = maze.isReachable(start, finish)
   let warmTime = getMonoTime() - now
 
   if success:
