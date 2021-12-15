38862f3cca160475cd3d46ea0e612e8b6baecd9e
Benjamin Shirley-Quirk <benjamin.shirley-quirk@geoslam.com>
2021-12-15 22:20:59 +0000

    various optimizations

--- src/maze_bench_nim.nim
+++ src/maze_bench_nim.nim
@@ -8,48 +8,44 @@ import
   point,
   mazesolver
 
-let image = readImage("maze1000.png")
-echo fmt"Loaded image: {image.width} x {image.height}"
+proc main() =
+  let image = readImage("maze1000.png")
+  echo fmt"Loaded image: {image.width} x {image.height}"
 
-echo "Transforming image into maze..."
-var now = getMonoTime()
+  echo "Transforming image into maze..."
 
-let maze = newSeq2D[bool](image.width, image.height)
+  var 
+    now = getMonoTime()
+    maze = newSeq2D(image.width, image.height)
+  
+  for i, rgba in image.data:
+    maze[i] = (rgba.r + rgba.g + rgba.b) > 0
+ 
+  echo fmt"Transformed image into maze in {getMonoTime() - now}"
 
-var
-  column: int
-  row: int
-for i, rgba in image.data:
-  maze[column, row] = (rgba.r + rgba.g + rgba.b) > 0
-  if column + 1 >= image.width:
-    column = 0
-    row += 1
+  func toIdx(p:(int,int)):Point = (p[0] + p[1] * image.width,p[0],p[1])
+  let
+    start = (0, 1).toIdx
+    finish = (2000, 1999).toIdx
+
+  echo fmt"Solving maze from ({start.x}, {start.y}) to ({finish.x}, {finish.y}) ..."
+  # Test "cold"
+  now = getMonoTime()
+  var success = maze.isReachable(start, finish)
+  let coldTime = getMonoTime() - now
+
+  # Warm up
+  for _ in 0..1000:
+    discard maze.isReachable(start, finish)
+
+  # Test "warm"
+  now = getMonoTime()
+  success = maze.isReachable(start, finish)
+  let warmTime = getMonoTime() - now
+
+  if success:
+    echo fmt"Solved maze in: cold={coldTime}, warm={warmTime}"
   else:
-    column += 1
+    echo "Failed to solve maze."
 
-echo fmt"Transformed image into maze in {getMonoTime() - now}"
-
-const 
-  start: Point = (0, 1)
-  finish: Point = (2000, 1999)
-
-echo fmt"Solving maze from ({start.x}, {start.y}) to ({finish.x}, {finish.y}) ..."
-# Test "cold"
-now = getMonoTime()
-var success = maze.isReachable(start, finish)
-let coldTime = getMonoTime() - now
-
-# Warm up
-for _ in 0..1000:
-  discard maze.isReachable(start, finish)
-
-# Test "warm"
-now = getMonoTime()
-success = maze.isReachable(start, finish)
-let warmTime = getMonoTime() - now
-
-if success:
-  echo fmt"Solved maze in: cold={coldTime}, warm={warmTime}"
-else:
-  echo "Failed to solve maze."
-
+main()
